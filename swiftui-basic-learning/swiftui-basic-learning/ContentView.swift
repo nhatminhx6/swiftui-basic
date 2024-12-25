adbd5bcb1bb5ef26e3cfee2082c3d46f85dd15cd
MinhTego <nn.minh@tego.global>
2024-12-25 15:23:08 +0700

    init project

---
+++ swiftui-basic-learning/swiftui-basic-learning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui-basic-learning
+//
+//  Created by Nhat Minh on 25/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui-basic-learning/swiftui-basic-learning/swiftui_basic_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_basic_learningApp.swift
+//  swiftui-basic-learning
+//
+//  Created by Nhat Minh on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_basic_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
